e345b12707bc7590a9bc06ab6c642cdc0b872ff8
Jose Nortes <nooors@gmail.com>
2022-08-24 17:26:26 +0200

    test:added an expect to form.component.espec

--- src/app/intermediate/forms/form.spec.ts
+++ src/app/intermediate/forms/form.spec.ts
@@ -1,24 +1,26 @@
-import { FormBuilder } from '@angular/forms';
-import { LoginForm } from './form';
-describe('Forms', () => {
+import { FormBuilder } from "@angular/forms";
+import { LoginForm } from "./form";
+describe("Forms", () => {
   let component: LoginForm;
 
   beforeEach(() => (component = new LoginForm(new FormBuilder())));
 
-  it('Should create a form with two fields, email and password', () => {
-    expect(component.form.contains('email')).toBeTruthy();
-    expect(component.form.contains('password')).toBeTruthy();
+  it("Should create a form with two fields, email and password", () => {
+    expect(component.form.contains("email")).toBeTruthy();
+    expect(component.form.contains("password")).toBeTruthy();
+    expect(Object.keys(component.form.controls).length).toBe(2);
+    console.log(component.form);
   });
 
-  it('Email field should be required', () => {
-    const control = component.form.get('email');
-    control?.setValue('');
+  it("Email field should be required", () => {
+    const control = component.form.get("email");
+    control?.setValue("");
     expect(control?.valid).toBeFalsy();
   });
 
-  it('The email field should have the correct email format', () => {
-    const control = component.form.get('email');
-    control?.setValue('lala@lala.com');
+  it("The email field should have the correct email format", () => {
+    const control = component.form.get("email");
+    control?.setValue("lala@lala.com");
     expect(control?.valid).toBeTruthy();
   });
 });
